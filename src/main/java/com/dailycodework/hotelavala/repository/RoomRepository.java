94a2a7971d9b8b5fd638c735ebc6704226db3eb1
marko <grozdanovicmarko170@gmail.com>
2024-07-22 13:49:44 +0200

    Finding available rooms for room search on UI

--- src/main/java/com/dailycodework/hotelavala/repository/RoomRepository.java
+++ src/main/java/com/dailycodework/hotelavala/repository/RoomRepository.java
@@ -4,10 +4,21 @@ import com.dailycodework.hotelavala.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
     @Query("select room.roomType "+
            "from Room room")
     List<String> getRoomTypes();
+
+
+    @Query(" SELECT r FROM Room r " +
+            " WHERE r.roomType LIKE %:roomType% " +
+            " AND r.id NOT IN (" +
+            "  SELECT br.room.id FROM BookedRoom br " +
+            "  WHERE ((br.checkInDate <= :checkOutDate) AND (br.checkOutDate >= :checkInDate))" +
+            ")")
+    List<Room> findAvailableRoomsByDatesAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
